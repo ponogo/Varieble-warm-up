3d8d78aa70105407050f90d0f96d7f046162f8b2
Pavleg <pajabonki@gmail.com>
2020-07-21 17:23:00 +0000

    the code

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-#fname = input("What is your first name:")
+  #fname = input("What is your first name:")
 #lname = input("what is your last name:")
 
 #print("Your name is " + lname + " " + fname)
 
 
 
-counter = 0
-for i in range(3):
-  counter += 1
-  print(counter)
+#counter = 0
+#for i in range(3):
+#  counter += 1
+#  print(counter)
+
+for x in range(20):
+  if x > 10:
+    print("less than ten", x)
+  elif x == 10:
+    print("x is ten")
+  else:
+    print("greater than ten", x)
