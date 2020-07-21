04abe631a674c09b5d8a007ce1a8807825b5adf9
Pavleg <pajabonki@gmail.com>
2020-07-21 17:00:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#fname = input("What is your first name:")
+#lname = input("what is your last name:")
+
+#print("Your name is " + lname + " " + fname)
+
+
+
+counter = 0
+for i in range(3):
+  counter += 1
+  print(counter)
